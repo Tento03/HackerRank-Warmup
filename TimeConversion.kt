17b24d7a7ed35cfe56ee4cba3c01b4590fe0b1b8
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-02 18:57:45 +0700

    Time Conversion

---
+++ TimeConversion.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+fun timeConversion(s: String): String {
+    // Write your code here
+    var amPm=s.takeLast(2)
+    var hour=s.substring(0,2).toInt()
+    var minute=s.substring(2,8)
+    
+    if(amPm=="AM" && hour==12){
+        hour=0
+    }
+    else if(amPm=="PM" && hour!=12){
+        hour+=12
+    }
+    return "%02d%s".format(hour,minute)
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val result = timeConversion(s)
+
+    println(result)
+}
