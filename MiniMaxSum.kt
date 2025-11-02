484eccff6e829d13285da8117b158adb5902f618
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-02 18:31:31 +0700

    Mini Max Sum

---
+++ MiniMaxSum.kt
@@ -0,0 +1,42 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun miniMaxSum(arr: Array<Int>): Unit {
+    // Write your code here
+    var longArr=arr.map { it.toLong() }
+    var totalArr=longArr.sum()
+    var maxValue=arr.max()
+    var minValue=arr.min()
+    var maxResult=totalArr-minValue
+    var minResult=totalArr-maxValue
+    println("$minResult $maxResult")
+}
+
+fun main(args: Array<String>) {
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    miniMaxSum(arr)
+}
