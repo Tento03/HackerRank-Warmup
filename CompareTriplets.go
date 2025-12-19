cd4f13a945d7a7ee6f2aae9a973270ce5a3f1e74
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-19 18:56:59 +0700

    Solve me first.go

--- CompareTriplets.go
+++ CompareTriplets.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-)
-
 /*
  * Complete the 'compareTriplets' function below.
  *
@@ -33,64 +24,64 @@ func compareTriplets(a []int32, b []int32) []int32 {
 	return totAB
 }
 
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+// func main() {
+// 	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
 
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
+// 	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+// 	checkError(err)
 
-	defer stdout.Close()
+// 	defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+// 	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
 
-	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+// 	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
 
-	var a []int32
+// 	var a []int32
 
-	for i := 0; i < 3; i++ {
-		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
-		checkError(err)
-		aItem := int32(aItemTemp)
-		a = append(a, aItem)
-	}
+// 	for i := 0; i < 3; i++ {
+// 		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+// 		checkError(err)
+// 		aItem := int32(aItemTemp)
+// 		a = append(a, aItem)
+// 	}
 
-	bTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+// 	bTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
 
-	var b []int32
+// 	var b []int32
 
-	for i := 0; i < 3; i++ {
-		bItemTemp, err := strconv.ParseInt(bTemp[i], 10, 64)
-		checkError(err)
-		bItem := int32(bItemTemp)
-		b = append(b, bItem)
-	}
+// 	for i := 0; i < 3; i++ {
+// 		bItemTemp, err := strconv.ParseInt(bTemp[i], 10, 64)
+// 		checkError(err)
+// 		bItem := int32(bItemTemp)
+// 		b = append(b, bItem)
+// 	}
 
-	result := compareTriplets(a, b)
+// 	result := compareTriplets(a, b)
 
-	for i, resultItem := range result {
-		fmt.Fprintf(writer, "%d", resultItem)
+// 	for i, resultItem := range result {
+// 		fmt.Fprintf(writer, "%d", resultItem)
 
-		if i != len(result)-1 {
-			fmt.Fprintf(writer, " ")
-		}
-	}
+// 		if i != len(result)-1 {
+// 			fmt.Fprintf(writer, " ")
+// 		}
+// 	}
 
-	fmt.Fprintf(writer, "\n")
+// 	fmt.Fprintf(writer, "\n")
 
-	writer.Flush()
-}
+// 	writer.Flush()
+// }
 
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
+// func readLine(reader *bufio.Reader) string {
+// 	str, _, err := reader.ReadLine()
+// 	if err == io.EOF {
+// 		return ""
+// 	}
 
-	return strings.TrimRight(string(str), "\r\n")
-}
+// 	return strings.TrimRight(string(str), "\r\n")
+// }
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+// func checkError(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
---
+++ SolveMeFirst.go
@@ -0,0 +1,14 @@
+package main
+
+func solveMeFirst(a uint32, b uint32) uint32 {
+	// Hint: Type return (a+b) below
+	total := a + b
+	return total
+}
+
+// func main() {
+// 	var a, b, res uint32
+// 	fmt.Scanf("%v\n%v", &a, &b)
+// 	res = solveMeFirst(a, b)
+// 	fmt.Println(res)
+// }
